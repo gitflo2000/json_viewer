cf8e37b0dc7c4df9301ede2a7883771d09befad6
gitflo2000 <36086323+gitflo2000@users.noreply.github.com>
2023-04-26 12:11:30 +0200

    Update viewer.py
    adding timestamp

--- viewer.py
+++ viewer.py
@@ -3,13 +3,16 @@ import matplotlib.pyplot as pyplot
 
 df = pd.read_json('data.json')
 
+df['Timestamp'] = df['Timestamp'].apply(lambda s:s.strip())
+df['Timestamp'] = pd.to_datetime(df['Timestamp'], format='%Y-%m-%d_%H-%M-%S')
+
 #Line Chart
 fig = pyplot.figure()
 ax1 = fig.add_subplot(1,1,1)
 ax1.set_xlabel('Time')
 ax1.set_ylabel('Temperature')
-df['T1'].plot(kind='line')
-df['T2'].plot(kind='line')
-df['T3'].plot(kind='line')
+pyplot.plot(df['Timestamp'],df['T1'])
+pyplot.plot(df['Timestamp'],df['T2'])
+pyplot.plot(df['Timestamp'],df['T3'])
 
 pyplot.show()
